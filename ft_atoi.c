f51afbb29cb083ffe7e975094043bfc7bd566b5a
tkankan42 <thkankn9@gmail.com>
2022-02-25 14:45:51 +0300

    fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,17 @@
 /*   By: tkankan <tkankan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 14:16:18 by tkankan           #+#    #+#             */
-/*   Updated: 2022/02/21 04:45:10 by tkankan          ###   ########.fr       */
+/*   Updated: 2022/02/25 14:44:00 by tkankan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include "limits.h"
 
 int	ft_atoi(const char *str)
 {
-	int	result;
-	int	negative;
-	int	i;
+	unsigned long	result;
+	int				negative;
+	int				i;
 
 	result = 0;
 	negative = 1;
@@ -31,30 +32,11 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = (str[i] - '0') + (result * 10);
-		if (result > 2147483647)
-			return (-1);
-		if (result < -2147483648)
-			return (0);
 		i++;
 	}
+	if (result > LONG_MAX && negative == -1)
+		return (0);
+	if (result > LONG_MAX && negative == 1)
+		return (-1);
 	return (result * negative);
 }
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main () {
-   int val;
-   char str[20];
-
-   strcpy(str, "--98993489");
-   val = ft_atoi(str);
-   printf("Yazı değer = %s, Sayı değer = %d\n", str, val);
-
-   strcpy(str, "-24484748210");
-   val = ft_atoi(str);
-   printf("Yazı değer = %s, Sayı değer = %d\n", str, val);
-
-   return(0);
-}*/
